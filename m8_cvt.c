6f681aa28d17d2d9db51ff748690c3ba6953f99e
K Viddya <62757458+viddya673@users.noreply.github.com>
2022-03-04 07:13:20 +0530

    Update m8_cvt.c

--- m8_cvt.c
+++ m8_cvt.c
@@ -1,17 +1,3 @@
-/*A simplified mapping of processes into a Cartesian virtual topology appears below:
- _______________________________
-|   0   |   1   |   2   |   3   |
-| (0,0) | (0,1) | (0,2) | (0,3) |
-|_______|_______|_______|_______|
-|   4   |   5   |   6   |   7   |
-| (1,1) | (1,2) | (1,3) | (1,4) |
-|_______|_______|_______|_______|
-|   8   |   9   |  10   |  11   |
-| (2,1) | (2,2) | (2,3) | (2,4) |
-|_______|_______|_______|_______|
-|   12  |   13  |   14  |   15  |
-| (3,1) | (3,2) | (3,3) | (3.4) |
-|_______|_______|_______|_______|*/
 #include <mpi.h>
 #include <stdio.h>
 #define SIZE 16
@@ -21,11 +7,9 @@
 #define RIGHT 3
 
 int main(int argc, char *argv[])  {
-int numtasks, rank, source, dest, outbuf, i,
-    nbrs[4], dims[2]={4,4},
-    periods[2]={0,0}, reorder=0, coords[2];
+int numtasks, rank, i, nbrs[4], dims[2]={4,4}, periods[2]={0,0}, reorder=0, coords[2];
 
-MPI_Comm cartcomm;   // required variable
+MPI_Comm cartcomm;
 
 MPI_Init(&argc,&argv);
 MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
